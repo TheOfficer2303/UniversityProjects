0acd99d816370a6d134ac964df9818ebb4f86d54
Blaž Solić <76702547+TheOfficer2303@users.noreply.github.com>
2022-05-08 20:10:39 +0200

    Delete Scripting Languages/Lab2 directory

--- Scripting Languages/Lab2/zd1.pl
+++
@@ -1,7 +0,0 @@
-print "Unesi niz\n";
-chomp($niz = <STDIN>);
-print "Unesi broj\n";
-chomp($broj = <STDIN>);
-
-$niz = "$niz\n" x $broj;
-print $niz;
--- Scripting Languages/Lab2/zd2.pl
+++
@@ -1,5 +0,0 @@
-chomp(@nums = <STDIN>);
-foreach $num (@nums) {
-    $sum += $num;
-}
-print "Mean: $sum\n";
